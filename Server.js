215027d7a35645606c46caba393ee344bfc9f645
KashanAdnan <khanzaidaboy@gmail.com>
2023-06-03 13:54:23 +0500

    some-code

--- Server.js
+++ Server.js
@@ -32,7 +32,7 @@ app.use("/api/v3", ContactRoute);
 
 app.use(ErrorMiddleware);
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server is Running`);
 });
 
